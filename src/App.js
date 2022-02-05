8e1d23040eec219353550b3dddc9914f8342ca81
DemonDucky <cuti1mat@gmail.com>
2022-02-05 12:36:03 +0700

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Body from "./Body"
+import { useState, useCallback } from "react"
+
+// const list = [
+//   {
+//     id: 0,
+//     value: 'test1',
+//   },
+//   {
+//     id: 1,
+//     value: 'test2',
+//   },
+//   {
+//     id: 2,
+//     value: 'test3',
+//   },
+//   {
+//     id: 3,
+//     value: 'test4',
+//   },
+// ]
+
+// function App() {
+
+//   const [check, setcheck] = useState([])
+
+//   function handleCheck(id) {
+
+//     if (check.includes(id)) {
+//       return setcheck(check.filter((value) => value != id ))
+//     }else {
+//       return setcheck([...check, id])
+//     }
+//   }
+
+//   function handleSubmit() {
+//     return console.log([check])
+//   }
+
+//   return (
+//     <div className="App">
+//       {list.map(function (value, index) {
+//         return <div key={index}>
+//           <input onChange={() => handleCheck(value.id)} type="checkbox" />{value.value}
+//         </div>
+//       })}
+//       <button onClick={handleSubmit}>Increase</button>
+//     </div>
+//   );
+// }
+
+// function App() {
+
+//   const [works, setWorks] = useState(JSON.parse(localStorage.getItem('works')))
+//   const [work, setWork] = useState('')
+
+//   function handleChange(e) {
+//     setWork(e.target.value)
+//   }
+
+//   function handleSubmit() {
+//     setWorks((prev) => {
+//       const newWorks = [...prev, work]
+
+//       localStorage.setItem('works', JSON.stringify(newWorks))
+
+//       return newWorks
+//     })
+
+//     setWork('')
+//   }
+
+//   return (
+//     <div className="App">
+//       <div>
+//         <input value={work} onChange={handleChange} type="text" />
+//         <button onClick={handleSubmit}>Submit</button>
+//       </div>
+//       <div>
+//         <ul>
+//           {works.map((value, index) => <li key={index}>{value}</li>)}
+//         </ul>
+//       </div>
+//     </div>
+//   )
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Body />
+        </>
+    )
 }
 
-export default App;
+export default App
---
+++ src/Body.js
@@ -0,0 +1,26 @@
+import { useState, useEffect, useRef, memo, useMemo, useReducer } from 'react'
+
+function calculate(state, action) {
+    switch (action) {
+        case 'up':
+            return state + 1
+        case 'down':
+            return state - 1
+        default:
+            throw new Error('Invalid action222')
+    }
+}
+
+function Body() {
+    const [number, setNumber] = useReducer(calculate, 0)
+
+    return (
+        <div className="body">
+            <h1>{number}</h1>
+            <button onClick={() => setNumber('up')}>Up</button>
+            <button onClick={() => setNumber('down')}>Down</button>
+        </div>
+    )
+}
+
+export default Body
